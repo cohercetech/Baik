43c6acbf8a7af5f57fc32626e1225a38e7cad137
Coherece Tech <cohercetech@gmail.com>
2020-06-08 15:08:01 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -10,7 +10,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the notification when someone subscribes
-    $emailTo = 'adnan.zulkarnain@gmail.com';
+    $emailTo = 'cohercetech@gmail.com';
 
     $subscriber_email = ($_POST['email']);
 
